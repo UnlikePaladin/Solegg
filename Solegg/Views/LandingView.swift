909eedad17ebf88e1ab510e35ec1b0397c00de0f
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-06-07 18:14:50 -0600

    Home view and tab view

---
+++ Solegg/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  Solegg
+//
+//  Created by Ernesto Garza Berrueto on 07/06/25.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
